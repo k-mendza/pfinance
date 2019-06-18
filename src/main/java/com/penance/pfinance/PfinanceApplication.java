b3f7a39933402ccd67aa215777b760687659bbc1
k-mendza <karolmendza@gmail.com>
2019-06-18 23:32:42 +0200

    initial commit

---
+++ src/main/java/com/penance/pfinance/PfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.penance.pfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfinanceApplication.class, args);
+    }
+
+}
