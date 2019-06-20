afc7793a42796ad8c3566a0302dc7c82f5dca121
K-Mendza <karolmendza@gmail.com>
2019-06-20 23:38:46 +0200

    added TRANSACTION and TRANS_CATEGORY tables to migration script

--- src/main/resources/db.migration/V1_init.sql
+++ src/main/resources/db.migration/V1_init.sql
@@ -15,3 +15,52 @@ TABLESPACE pg_default;
 
 ALTER TABLE public."USERS"
     OWNER to postgres;
+
+CREATE TABLE public."TRANS_CATEGORY"
+(
+    "ID" bigserial NOT NULL,
+    "NAME" character varying NOT NULL,
+    "DESCRIPTION" character varying,
+    "CREATION_DATE" timestamp with time zone,
+    PRIMARY KEY ("ID")
+)
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+ALTER TABLE public."TRANS_CATEGORY"
+    OWNER to postgres;
+
+CREATE TABLE public."TRANS_CATEGORY"
+(
+    "ID" bigserial NOT NULL,
+    "NAME" character varying NOT NULL,
+    "DESCRIPTION" character varying,
+    "CREATION_DATE" timestamp with time zone,
+    PRIMARY KEY ("ID")
+)
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+ALTER TABLE public."TRANS_CATEGORY"
+    OWNER to postgres;
+
+CREATE TABLE public."TRANSACTION"
+(
+    "ID" bigserial NOT NULL,
+    "DESCRIPTION" character varying,
+    "AMOUNT" decimal NOT NULL,
+    "CREATION_DATE" timestamp with time zone,
+    "CATEGORY_ID" bigserial NOT NULL ,
+    PRIMARY KEY ("ID")
+)
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
+
+ALTER TABLE public."TRANS_CATEGORY"
+    OWNER to postgres;
