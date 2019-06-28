8a2ece8ecf64d20befb6105e2e6bb75ebc0b2daf
KarMen <karolmendza@gmail.com>
2019-06-28 22:47:23 +0200

    Added ExpenseCategory entity

---
+++ src/main/java/com/penance/pfinance/model/ExpenseCategory.java
@@ -0,0 +1,33 @@
+package com.penance.pfinance.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "expense_category")
+public class ExpenseCategory {
+
+    @Id
+    @Column(name = "id", columnDefinition = "NUMERIC(19,2)")
+    @SequenceGenerator(name="seq", sequenceName="expense_category_id_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "creation_date", columnDefinition = "TIMESTAMP")
+    private LocalDate creationDate;
+}
