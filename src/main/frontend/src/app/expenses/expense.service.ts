1d3d1eb4966ec97b1c863b7f745ce2a240be2317
KarMen <karolmendza@gmail.com>
2019-07-02 23:25:58 +0200

    changes to table for new data model

--- src/main/frontend/src/app/expenses/expense.service.ts
+++ src/main/frontend/src/app/expenses/expense.service.ts
@@ -1,25 +1,8 @@
 import {Injectable} from "@angular/core";
-import {Expense} from "./expense.model";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class ExpenseService {
-  private expenses: Expense[] = [
-    new Expense(
-      1,
-      'myLogin@com.com',
-      'Payee',
-      'CreditCard',
-      'Title',
-      'desc',
-      19.99,
-      'PLN',
-      '2019-06-30',
-      '2019-06-30',
-      'category',
-      'subcategory'
-    )
-  ];
 
   constructor(private http: HttpClient){
 
--- src/main/frontend/src/app/expenses/expenses.component.ts
+++ src/main/frontend/src/app/expenses/expenses.component.ts
@@ -3,24 +3,37 @@ import {ExpenseService} from "./expense.service";
 import {Expense} from "./expense.model";
 import {Subscription} from "rxjs";
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface ExpenseElement {
+  id: number;
+  appUserLogin: string;
+  payeeName: string;
+  paySourceName: string;
+  title: string;
+  description: string;
+  amount: number;
+  currencyId: string;
+  paymentDate: string;
+  creationDate: string;
+  categoryName: string;
+  subcategoryName: string;
+
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const EXPENSE_DATA: ExpenseElement[] = [
+  {
+    id: 1,
+    appUserLogin: 'Login',
+    payeeName: 'Payee',
+    paySourceName: 'PaySourceName',
+    title: 'title',
+    description: 'desc',
+    amount: 99.99,
+    currencyId: 'PLN',
+    paymentDate: '2019-07-02',
+    creationDate: '2019-07-02',
+    categoryName: 'Category',
+    subcategoryName: 'Subcategory'
+  },
 ];
 
 /**
@@ -32,8 +45,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'expenses.component.html',
 })
 export class ExpensesComponent implements OnInit,OnDestroy{
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id','title', 'amount', 'currencyId', 'paymentDate','categoryName','subcategoryName'];
+  dataSource = EXPENSE_DATA;
 
   getExpensesSub: Subscription;
 
