673e2a09478f135874e3cb1969d56c602f4d0031
KarMen <karolmendza@gmail.com>
2019-07-06 12:28:54 +0200

    ExpenseController update

--- src/main/java/com/penance/pfinance/controllers/ExpenseController.java
+++ src/main/java/com/penance/pfinance/controllers/ExpenseController.java
@@ -5,15 +5,12 @@ import com.penance.pfinance.services.ExpenseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/expenses/")
+@RequestMapping("/api/expenses")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
@@ -29,8 +26,16 @@ public class ExpenseController {
         return new ResponseEntity<>( dtoList, HttpStatus.OK);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ExpenseDTO> getExpenseById(@PathVariable Long id){
         return new ResponseEntity<>(expenseService.getExpenseById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExpense(@PathVariable Long id){
+
+        expenseService.deleteExpenseById(id);
+
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 }
