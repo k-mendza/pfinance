bdeba153acb1660f4f3c776c041ee222139c4a56
k-mendza <karolmendza@gmail.com>
2019-07-13 17:52:53 +0200

    added swagger

--- src/main/java/com/penance/pfinance/api/v1/DTO/TransactionListDTO.java
+++ src/main/java/com/penance/pfinance/api/v1/DTO/TransactionListDTO.java
@@ -1,10 +1,14 @@
 package com.penance.pfinance.api.v1.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionListDTO {
 
     private List<TransactionDTO> transactionDTOList;
