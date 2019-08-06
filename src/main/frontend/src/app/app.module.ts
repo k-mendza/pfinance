646374b8be85632ac98373f22c225f40677270f5
Karol Mendza <karolmendza@gmail.com>
2019-08-06 23:40:19 +0200

    Frontend reset

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,38 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ExpenseService} from "./expenses/expense.service";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatTableModule} from '@angular/material/table';
-import {ExpensesComponent} from "./expenses/expenses.component";
 import {HttpClientModule} from "@angular/common/http";
-import { ExpensesTableComponent } from './expenses/expenses-table/expenses-table.component';
-import { MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
-import { ExpensesDashboardComponent } from './expenses/expenses-dashboard/expenses-dashboard.component';
-import { LayoutModule } from '@angular/cdk/layout';
+import { TransactionComponent } from './transaction/transaction.component';
+import {MaterialModule} from "./material.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpensesComponent,
-    ExpensesTableComponent,
-    ExpensesDashboardComponent
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule,
     HttpClientModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule
+    MaterialModule
   ],
-  providers: [ExpenseService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
